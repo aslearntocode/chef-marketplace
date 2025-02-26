226948410a72db1aba4eea9bbedf4574574a357e
aslearntocode <aslearntocode@gmail.com>
2025-02-26 16:16:52 +0530

    Added Log-In

---
+++ types/chef.ts
@@ -0,0 +1,18 @@
+export interface Chef {
+  id: number;
+  name: string;
+  image: string;
+  specialty: string;
+  location: string;
+  description: string;
+  rating: number;
+  deliveryAreas: string[];
+  menu: {
+    [category: string]: {
+      id: number;
+      name: string;
+      price: number;
+      description: string;
+    }[];
+  };
+} 
