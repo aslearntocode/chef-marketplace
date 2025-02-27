6f209584b4e6db4aa2199c761644dc2901ac6e8c
aslearntocode <aslearntocode@gmail.com>
2025-02-27 10:21:27 +0530

    Added Cart Page

--- types/menu.ts
+++ types/menu.ts
@@ -6,9 +6,4 @@ export interface MenuItem {
   category?: string;
 }
 
-export interface CartMenuItem extends MenuItem {
-  chefId?: number;
-  bakerId?: number;
-  chefName?: string;
-  bakerName?: string;
-} 
+// Remove CartMenuItem if it's not being used 
