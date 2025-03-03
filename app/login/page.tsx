1553371a7a881b23129d3fcd4bfd1d16adb27580
aslearntocode <aslearntocode@gmail.com>
2025-03-03 18:31:16 +0530

    Fixed Login Redirect

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useAuth } from '@/context/AuthContext';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { FirebaseError } from 'firebase/app';
